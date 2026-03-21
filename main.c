d516cfcb0ad430622c9e8edb904b1b69d34153a8
HitGub78 <kometa78228@gmail.com>
2026-03-21 17:38:55 -0400

    working version

--- main.c
+++ main.c
@@ -50,16 +50,15 @@ int pop(queue *Q){
     return ret;
 }
 
-void BFS(int start, int *ar, int bw_size, int *CSR, int *vis, unsigned char *grey) {
+//standart BFS
+void BFS(int start, int *ar, int bw_size, int *CSR, int *vis, unsigned char *grey, int *count) {
     queue *Q = init();
     queue *connected = init();
     int num;
     push(start, Q);
     push(start, connected);
     vis[start] = 1;
-    int total = 0;
     while(Q->size) {
-        total+=1;
         num = pop(Q);
         if (num < bw_size-1) {
             for(int i = ar[num]; i < ar[num+1]; i++) {
@@ -71,22 +70,26 @@ void BFS(int start, int *ar, int bw_size, int *CSR, int *vis, unsigned char *gre
             }
         }
     }
-    if (connected->size > 10){
+    // anything consisting of this much pixels is likely not a tanker
+    if (connected->size > 8){
         while(connected->size){
             int s = pop(connected);
             grey[s] = 0;
         }
+    }
+    else{
+        *count += 1;
     }
     free(connected);
     free(Q->ar);
     free(Q);
 }
 
-void graphify(unsigned char *grey, int width, int height) {
+int graphify(unsigned char *grey, int width, int height) {
     int bw_size = height * width;
     int *ar = malloc(bw_size * sizeof(int));
     int total = 0;
-    
+    // standart CSR graph creation
     for(int i = 0; i < bw_size; i++) {
         ar[i] = total;
         int neighbours = 0;
@@ -120,14 +123,21 @@ void graphify(unsigned char *grey, int width, int height) {
             }
         }
     }
+    int count = 0;
+
     int *vis = calloc(bw_size,sizeof(int));
+    // if we find a lump of pixels connected to each other, we either remove it from the 
+    // picture, or count it as a ship
     for(int i = 0; i < bw_size; i++){
-        if(!vis[i]) BFS(i, ar, bw_size, car, vis, grey);
+        if(!vis[i] && grey[i] > 0){
+            BFS(i, ar, bw_size, car, vis, grey, &count);
+        }
     }
-    
+
     free(ar);
     free(car);
     free(vis);
+    return count;
 }
 
 // принимаем на вход: имя файла, указатели на int для хранения прочитанной ширины и высоты картинки
@@ -158,13 +168,15 @@ void write_png(const char* filename, const unsigned char* image, unsigned width,
   free(png);
 }
 
+//turns rgb picture into black and white for which each pixel 
+// is determined by 1 number
 void blackwhite(unsigned char *col, unsigned char *grey, int bw_size){
     for(int i = 0; i < bw_size; i++){
         grey[i] = (unsigned char)(0.299*col[i*4] + 0.587*col[i*4+1] + 0.114*col[i*4+2]);        
     }
     return;
 }
-
+// turns black and white picture to "colored" for write_png to work
 void torgb(unsigned char *col, unsigned char *grey, int bw_size){
     for(int i = 0; i < bw_size; i++){
         col[4*i] = grey[i];
@@ -180,13 +192,19 @@ void contrast(unsigned char *col, int bw_size)
     int i; 
     for(i=0; i < bw_size; i++)
     {
-        if(col[i] < 55)
-        col[i] = 0; 
-        if(col[i] >= 195)
+        if(col[i] >= 100)
         col[i] = 255;
     } 
     return; 
 } 
+// for simplicity
+void radical(unsigned char *col, int bw_size){
+    for(int i = 0; i < bw_size; i++){
+        if (col[i] > 30) col[i] = 255;
+        else col[i] = 0;
+    }
+    
+}
 
 // Гауссово размыттие
 void Gauss_blur(unsigned char *col, unsigned char *blr_pic, int width, int height)
@@ -195,11 +213,16 @@ void Gauss_blur(unsigned char *col, unsigned char *blr_pic, int width, int heigh
     for(i=1; i < height-1; i++) 
         for(j=1; j < width-1; j++)
         { 
-            blr_pic[width*i+j] = 0.084*col[width*i+j] + 0.084*col[width*(i+1)+j] + 0.084*col[width*(i-1)+j]; 
-            blr_pic[width*i+j] = blr_pic[width*i+j] + 0.084*col[width*i+(j+1)] + 0.084*col[width*i+(j-1)]; 
-            blr_pic[width*i+j] = blr_pic[width*i+j] + 0.063*col[width*(i+1)+(j+1)] + 0.063*col[width*(i+1)+(j-1)]; 
-            blr_pic[width*i+j] = blr_pic[width*i+j] + 0.063*col[width*(i-1)+(j+1)] + 0.063*col[width*(i-1)+(j-1)]; 
-        } 
+            blr_pic[width*i+j] = 0.111*col[width*i+j] +           
+                                0.111*col[width*(i+1)+j] +    
+                                0.111*col[width*(i-1)+j] + 
+                                0.111*col[width*i+(j+1)] + 
+                                0.111*col[width*i+(j-1)] + 
+                                0.111*col[width*(i+1)+(j+1)] + 
+                                0.111*col[width*(i+1)+(j-1)] + 
+                                0.111*col[width*(i-1)+(j+1)] + 
+                                0.111*col[width*(i-1)+(j-1)];
+        }
    return; 
 } 
 
@@ -238,21 +261,44 @@ int main()
     unsigned char* blr_pic = (unsigned char*)malloc(size*sizeof(unsigned char)); 
     unsigned char* contr = (unsigned char*)malloc(size*sizeof(unsigned char)); 
     unsigned char* grey = (unsigned char*)malloc(bw_size*sizeof(unsigned char));
+    unsigned char *blr_grey = malloc(bw_size*sizeof(unsigned char));
 
     blackwhite(picture, grey, bw_size);
-    Gauss_blur(grey, blr_pic, width, height);
-    torgb(picture, grey, bw_size);
-    // Black and white image
-    write_png("grey.png", picture, width, height);
-    torgb(picture, blr_pic, bw_size);
-    // Blurred black and white image
-    write_png("gauss.png", picture, width, height);
-    contrast(picture, size);
-    // Contrasted after blurring
-    write_png("contrast.png", picture, width, height);
+    torgb(contr, grey, bw_size);
+    contrast(contr, size);
     
-    graphify(grey, width, height);
+    // Contrasted 
+    write_png("contrast.png", contr, width, height);
+    blackwhite(picture, grey, bw_size);
+    for(int i = 0; i < 10; i++){
+        Gauss_blur(grey, blr_grey, width, height);
+        for(int j = 0; j < bw_size; j++) grey[j] = blr_grey[j];
+    }
+    torgb(blr_pic, blr_grey, bw_size);
+    // blurred (original)
+    write_png("gauss.png", blr_pic, width, height);
+    blackwhite(contr, grey, bw_size);
+    // subtract the blurred from contrasted, in hopes of removing the clouds
+    for(int i = 0; i < bw_size; i++){
+        int value = (int)grey[i] - (int)blr_grey[i];
+        grey[i] = (value > 0) ? value : 0; 
+    }
+    radical(grey, bw_size);
+    torgb(picture, grey, bw_size);
+    write_png("intermediate_result.png", picture, width, height);
+    // manually clearing areas with no ships
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width; j++){
+            if (j <= 500 || (j > 720 && i < 250) || i < 5 || i > (height-5))
+            grey[width*i+j] = 255;
+        }
+    }
+    int tankers = graphify(grey, width, height);
+    torgb(picture, grey, bw_size);
+    // to make sure modified pictures mostly includes all the same dots as the original
+    write_png("picture_out.png", picture, width, height);
 
+    printf("%d tankers are in the strait\n", tankers);
     
     
     free(grey); 
